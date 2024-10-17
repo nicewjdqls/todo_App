1fa5394559b882add86ee2134887e371db144300
koo <nicewdjql@naver.com>
2024-10-17 16:08:49 +0900

    Fix line endings

---
+++ controller/task.controller.js
@@ -0,0 +1,59 @@
+const Task = require("../model/Task");
+const taskController = {};
+
+taskController.createTask = async (req, res) => {
+    try {
+        const { task, isComplete } = req.body;
+        const newTask = new Task({ task, isComplete }); 
+        await newTask.save();
+        res.status(200).json({ status: 'ok', data: newTask });
+    } catch (err) {
+        res.status(400).json({ status: 'fail', error: err });
+    }
+};
+
+taskController.getTask = async (req, res) => {
+    try {
+        const taskList = await Task.find({}).select("-__v");
+        res.status(200).json({ status: "ok", data: taskList });
+    } catch (err) {
+        res.status(400).json({ status: 'fail', error: err });
+    }
+};
+
+taskController.putTask = async (req, res) => {
+    try {
+        const  id = req.params.id;
+        const { task, isComplete } = req.body;
+
+        const updateTask = await Task.updateOne(
+            { _id: id },
+            { task, isComplete }
+        );
+
+        if (updateTask.nModified === 0) {
+            return res.status(404).json({ status: 'fail', message: 'Task not found or no changes made' });
+        }
+
+        res.status(200).json({ status: 'ok', message: 'Task updated successfully' });
+    } catch (err) {
+        res.status(400).json({ status: 'fail', error: err });
+    }
+};
+
+taskController.delTask = async (req, res) => {
+    try {
+        const  id = req.params.id;
+
+        const delTask = await Task.deleteOne({ _id: id });
+
+        if (delTask.deletedCount === 0) {
+            return res.status(404).json({ status: 'fail', message: 'Task not found' });
+        }
+
+        res.status(200).json({ status: 'ok', message: 'Task deleted successfully' });
+    } catch (err) {
+        res.status(400).json({ status: 'fail', error: err });
+    }
+};
+module.exports = taskController;
---
+++ routes/task.api.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const taskController = require('../controller/task.controller');
+const router = express.Router();
+
+router.post('/', taskController.createTask);
+
+router.get('/',taskController.getTask);
+
+
+router.put('/:id', taskController.putTask);
+
+router.delete('/:id',taskController.delTask);
+
+
+module.exports = router;
