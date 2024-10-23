0b7a00460c3bd58874738061ca24ae4275540d28
koo <nicewdjql@naver.com>
2024-10-23 15:25:39 +0900

    finally update

--- routes/task.api.js
+++ routes/task.api.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const taskController = require('../controller/task.controller');
+const authController = require('../controller/auth.controller');
 const router = express.Router();
 
-router.post('/', taskController.createTask);
+router.post('/',authController.authenticate, taskController.createTask);
 
-router.get('/',taskController.getTask);
+router.get('/', authController.authenticate, taskController.getTask);
 
 
 router.put('/:id', taskController.putTask);
